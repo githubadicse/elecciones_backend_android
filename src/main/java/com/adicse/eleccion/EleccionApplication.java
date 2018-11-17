f55dc5556c597a1b7e78500cca37031dce935dda
crojasadicse <crojas.adicse@gmail.com>
2018-11-16 22:53:08 -0500

    Inicio

---
+++ src/main/java/com/adicse/eleccion/EleccionApplication.java
@@ -0,0 +1,12 @@
+package com.adicse.eleccion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EleccionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EleccionApplication.class, args);
+	}
+}
